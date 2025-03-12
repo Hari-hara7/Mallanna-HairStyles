5378223ae88d12d6751fa8f16c381a23ccdaafd1
Your Name <Hariharanath247@gmail.com>
2025-03-12 21:48:37 +0530

    hari

--- shop/src/components/Hero.tsx
+++ shop/src/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      {/* Darghjjhk Overlay */}
+      {/* Darghjjhk ghjhOverlay */}
       <div className="absolute inset-0 bg-black bg-opacity-70"></div>
 
       {/* Soft Glow Effects */}
